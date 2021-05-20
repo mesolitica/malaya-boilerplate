d038a8e0a08185eed3883bdbd091657d2ad02a95
husein zolkepli <husein.zol05@gmail.com>
2021-05-21 01:35:03 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -4,11 +4,6 @@ import setuptools
 __packagename__ = 'malaya-boilerplate'
 
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
-
 with open('requirements.txt') as fopen:
     req = list(filter(None, fopen.read().split('\n')))
 
@@ -18,7 +13,6 @@ setuptools.setup(
     version = '0.0.1',
     python_requires = '>=3.6.*',
     description = 'Tensorflow freeze graph optimization and boilerplates to share among Malaya projects.',
-    long_description = readme(),
     author = 'huseinzol05',
     author_email = 'husein.zol05@gmail.com',
     url = 'https://github.com/huseinzol05/malaya-boilerplate',
