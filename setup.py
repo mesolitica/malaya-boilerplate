c5481d480bd9b6f0bc825cda8a5937d9e79662a9
husein zolkepli <husein.zol05@gmail.com>
2021-05-21 14:00:59 +0800

    added github actions

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as fopen:
 setuptools.setup(
     name = __packagename__,
     packages = setuptools.find_packages(),
-    version = '0.0.1',
+    version = '0.0.2',
     python_requires = '>=3.6.*',
     description = 'Tensorflow freeze graph optimization and boilerplates to share among Malaya projects.',
     author = 'huseinzol05',
